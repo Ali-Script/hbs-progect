cb6f2911fb624756cf5b45fdf202059faa3295f0
Ali-Script <ali.prg01@gmail.com>
2024-01-10 17:35:52 +0330

    ud

--- app.js
+++ app.js
@@ -10,14 +10,22 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use("/course/cover", express.static(path.join(__dirname, 'public', 'course', 'cover')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname))
 //Defult Route ^
 
+const users = [
+    { id: 1, name: "John Doe" },
+    { id: 2, name: "Jane Smith" }
+
+]
+
 app.get("/", async (req, res) => {
-
+    res.render("views/index.hbs", { users })
+})
+app.post("/", async (req, res) => {
     res.render("views/index.hbs")
-
 })
 
 // Manual Route^
